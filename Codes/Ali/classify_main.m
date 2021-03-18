43de19a659d7f8c6f220a0326258b2c02c7fd2b6
Alison Peard <alison@Alisons-MacBook-Pro.local>
2021-03-18 18:15:28 +0000

    made plots .png

--- Codes/Ali/classify_main.m
+++ Codes/Ali/classify_main.m
@@ -26,22 +26,22 @@ Y = sin((1:n))'*ones(1,n);
 % end
 % 
 %  % 3(c) simulate amp_chim
-% ran1 = randi([1 n/2],1);
-% ran2 = randi([n/2 n],1);
-% size = ran2 - ran1 + 1;
-% eps = rand(2,size);
-% for i = 1:size
-%     shift = ceil(0.1*sin(4*pi*i/size));     % add small sinusoidal shift
-%     Y(:, ran1 + i - 1) = eps(1,i) + eps(2,i).*Y(:, ran1 + i - 1);
-%     Y(:, ran1 + i - 1) = circshift(Y(:, ran1 + i - 1), shift);
-% end
+ran1 = randi([1 n/2],1);
+ran2 = randi([n/2 n],1);
+size = ran2 - ran1 + 1;
+eps = rand(2,size);
+for i = 1:size
+    shift = ceil(0.1*sin(4*pi*i/size));     % add small sinusoidal shift
+    Y(:, ran1 + i - 1) = eps(1,i) + eps(2,i).*Y(:, ran1 + i - 1);
+    Y(:, ran1 + i - 1) = circshift(Y(:, ran1 + i - 1), shift);
+end
 % 
 % % 3(c)(ii) simulate amp chimera and death
-% rnd = rand(1,n);
-% rnd =  find(rnd > 0.6);
-% for i =1:length(rnd)
-%     Y(:,rnd(i)) = zeros(n,1);
-% end
+rnd = rand(1,n);
+rnd =  find(rnd > 0.6);
+for i =1:length(rnd)
+    Y(:,rnd(i)) = zeros(n,1);
+end
 
 %%
 
@@ -57,4 +57,4 @@ title(classx,'fontweight','bold','fontsize',16);
 xlabel('node index','fontsize',16);
 ylabel('Time','fontsize',16);
 
-%saveas(gcf,'classifier results/cooked example: ' + classx,'jpeg')
+saveas(gcf,'classifier results/' + classx,'png')
